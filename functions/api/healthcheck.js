dd8448ae680b7dfe9db3950874d080cdbe44c402
bestiecollabs <collabsbestie@gmail.com>
2025-10-08 20:48:09 -0700

    Add /api/healthcheck test endpoint

---
+++ functions/api/healthcheck.js
@@ -0,0 +1,7 @@
+﻿export async function onRequest({ request }) {
+  const url = new URL(request.url);
+  return new Response(
+    JSON.stringify({ ok: true, host: url.host, path: url.pathname }),
+    { headers: { "content-type": "application/json" } }
+  );
+}
