04ccccbfc3c18b1f1c4a5273fa23663489e881ff
bestiecollabs <collabsbestie@gmail.com>
2025-10-08 23:06:12 -0700

    deploy verify

--- functions/api/healthcheck.js
+++ functions/api/healthcheck.js
@@ -1,4 +1,4 @@
-﻿export async function onRequest({ request }) {
+export async function onRequest({ request }) {
   const url = new URL(request.url);
   return new Response(
     JSON.stringify({ ok: true, host: url.host, path: url.pathname }),
