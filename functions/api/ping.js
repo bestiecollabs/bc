180725f08f8117b79f1d7525827af5d9baec3c85
bestiecollabs <collabsbestie@gmail.com>
2025-10-13 14:55:26 -0700

    Add /api/ping to confirm Functions domain [10/13 13:06]

---
+++ functions/api/ping.js
@@ -0,0 +1,6 @@
+﻿export async function onRequestGet() {
+  return new Response(JSON.stringify({ ok: true, route: "/api/ping" }), {
+    status: 200,
+    headers: { "content-type": "application/json", "cache-control": "no-store" },
+  });
+}
