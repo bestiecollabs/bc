521e54a8f9e46193e6d40371d21f165517c47b9f
bestiecollabs <collabsbestie@gmail.com>
2025-10-13 15:10:11 -0700

    Add /api/admin/debug-env to inspect ADMIN_ALLOWLIST

---
+++ functions/api/admin/debug-env.js
@@ -0,0 +1,6 @@
+﻿export async function onRequestGet(context) {
+  const { env } = context;
+  const val = env.ADMIN_ALLOWLIST || "";
+  const summary = { ok:true, has: !!val, length: val.length, sample: val.slice(0, 5) };
+  return new Response(JSON.stringify(summary), { status: 200, headers: { "content-type":"application/json" }});
+}
