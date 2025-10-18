b7053a1e7e465412bf2cf573b81d3f34d410183a
bestiecollabs <collabsbestie@gmail.com>
2025-10-17 22:40:08 -0700

    pages(functions): global middleware to append charset=utf-8 for HTML responses

---
+++ functions/_middleware.js
@@ -0,0 +1,14 @@
+﻿export async function onRequest(context) {
+  // Pass through to static asset or route first
+  const res = await context.next();
+
+  // If HTML, ensure charset on Content-Type
+  const ct = res.headers.get("content-type") || "";
+  if (ct.startsWith("text/html") && !/charset=/i.test(ct)) {
+    const hdrs = new Headers(res.headers);
+    hdrs.set("content-type", "text/html; charset=utf-8");
+    return new Response(res.body, { status: res.status, headers: hdrs });
+  }
+
+  return res;
+}
